dfa77d91f9de0870bf57848208c006ab8f367705
Henry Perez <56861479+Henrypg92@users.noreply.github.com>
2024-01-14 23:33:09 -0600

    Subida Calculadora Básica
    Calculadora simple, suma, resta, multiplica, divide y muestra el historial de la última operación realizada.

---
+++ WF_PRINCIPAL/WF_PRINCIPAL.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace WF_PRINCIPAL
+{
+    partial class WF_PRINCIPAL
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WF_PRINCIPAL));
+            this.BTN_3 = new System.Windows.Forms.Button();
+            this.TXT_PANTALLA = new System.Windows.Forms.TextBox();
+            this.BTN_2 = new System.Windows.Forms.Button();
+            this.BTN_1 = new System.Windows.Forms.Button();
+            this.BTN_6 = new System.Windows.Forms.Button();
+            this.BTN_5 = new System.Windows.Forms.Button();
+            this.BTN_4 = new System.Windows.Forms.Button();
+            this.BTN_9 = new System.Windows.Forms.Button();
+            this.BTN_8 = new System.Windows.Forms.Button();
+            this.BTN_7 = new System.Windows.Forms.Button();
+            this.BTN_PUNTO = new System.Windows.Forms.Button();
+            this.BTN_0 = new System.Windows.Forms.Button();
+            this.BTN_LIMPIAR = new System.Windows.Forms.Button();
+            this.BTN_IGUAL = new System.Windows.Forms.Button();
+            this.BTN_RESTA = new System.Windows.Forms.Button();
+            this.BTN_MULTI = new System.Windows.Forms.Button();
+            this.BTN_DIVI = new System.Windows.Forms.Button();
+            this.BTN_SUMA = new System.Windows.Forms.Button();
+            this.BTN_IMPRIMIR = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BTN_3
+            // 
+            this.BTN_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_3.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_3.Location = new System.Drawing.Point(152, 312);
+            this.BTN_3.Name = "BTN_3";
+            this.BTN_3.Size = new System.Drawing.Size(64, 64);
+            this.BTN_3.TabIndex = 6;
+            this.BTN_3.Text = "3";
+            this.BTN_3.UseVisualStyleBackColor = false;
+            this.BTN_3.Click += new System.EventHandler(this.BTN_3_Click);
+            // 
+            // TXT_PANTALLA
+            // 
+            this.TXT_PANTALLA.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_PANTALLA.Location = new System.Drawing.Point(12, 12);
+            this.TXT_PANTALLA.Multiline = true;
+            this.TXT_PANTALLA.Name = "TXT_PANTALLA";
+            this.TXT_PANTALLA.Size = new System.Drawing.Size(274, 84);
+            this.TXT_PANTALLA.TabIndex = 21;
+            this.TXT_PANTALLA.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // BTN_2
+            // 
+            this.BTN_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_2.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_2.Location = new System.Drawing.Point(82, 312);
+            this.BTN_2.Name = "BTN_2";
+            this.BTN_2.Size = new System.Drawing.Size(64, 64);
+            this.BTN_2.TabIndex = 22;
+            this.BTN_2.Text = "2";
+            this.BTN_2.UseVisualStyleBackColor = false;
+            this.BTN_2.Click += new System.EventHandler(this.BTN_2_Click);
+            // 
+            // BTN_1
+            // 
+            this.BTN_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_1.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_1.Location = new System.Drawing.Point(12, 312);
+            this.BTN_1.Name = "BTN_1";
+            this.BTN_1.Size = new System.Drawing.Size(64, 64);
+            this.BTN_1.TabIndex = 23;
+            this.BTN_1.Text = "1";
+            this.BTN_1.UseVisualStyleBackColor = false;
+            this.BTN_1.Click += new System.EventHandler(this.BTN_1_Click);
+            // 
+            // BTN_6
+            // 
+            this.BTN_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_6.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_6.Location = new System.Drawing.Point(152, 242);
+            this.BTN_6.Name = "BTN_6";
+            this.BTN_6.Size = new System.Drawing.Size(64, 64);
+            this.BTN_6.TabIndex = 24;
+            this.BTN_6.Text = "6";
+            this.BTN_6.UseVisualStyleBackColor = false;
+            this.BTN_6.Click += new System.EventHandler(this.BTN_6_Click);
+            // 
+            // BTN_5
+            // 
+            this.BTN_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_5.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_5.Location = new System.Drawing.Point(82, 242);
+            this.BTN_5.Name = "BTN_5";
+            this.BTN_5.Size = new System.Drawing.Size(64, 64);
+            this.BTN_5.TabIndex = 25;
+            this.BTN_5.Text = "5";
+            this.BTN_5.UseVisualStyleBackColor = false;
+            this.BTN_5.Click += new System.EventHandler(this.BTN_5_Click);
+            // 
+            // BTN_4
+            // 
+            this.BTN_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_4.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_4.Location = new System.Drawing.Point(12, 242);
+            this.BTN_4.Name = "BTN_4";
+            this.BTN_4.Size = new System.Drawing.Size(64, 64);
+            this.BTN_4.TabIndex = 26;
+            this.BTN_4.Text = "4";
+            this.BTN_4.UseVisualStyleBackColor = false;
+            this.BTN_4.Click += new System.EventHandler(this.BTN_4_Click);
+            // 
+            // BTN_9
+            // 
+            this.BTN_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_9.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_9.Location = new System.Drawing.Point(152, 172);
+            this.BTN_9.Name = "BTN_9";
+            this.BTN_9.Size = new System.Drawing.Size(64, 64);
+            this.BTN_9.TabIndex = 27;
+            this.BTN_9.Text = "9";
+            this.BTN_9.UseVisualStyleBackColor = false;
+            this.BTN_9.Click += new System.EventHandler(this.BTN_9_Click);
+            // 
+            // BTN_8
+            // 
+            this.BTN_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_8.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_8.Location = new System.Drawing.Point(82, 172);
+            this.BTN_8.Name = "BTN_8";
+            this.BTN_8.Size = new System.Drawing.Size(64, 64);
+            this.BTN_8.TabIndex = 28;
+            this.BTN_8.Text = "8";
+            this.BTN_8.UseVisualStyleBackColor = false;
+            this.BTN_8.Click += new System.EventHandler(this.BTN_8_Click);
+            // 
+            // BTN_7
+            // 
+            this.BTN_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_7.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_7.Location = new System.Drawing.Point(12, 172);
+            this.BTN_7.Name = "BTN_7";
+            this.BTN_7.Size = new System.Drawing.Size(64, 64);
+            this.BTN_7.TabIndex = 29;
+            this.BTN_7.Text = "7";
+            this.BTN_7.UseVisualStyleBackColor = false;
+            this.BTN_7.Click += new System.EventHandler(this.BTN_7_Click);
+            // 
+            // BTN_PUNTO
+            // 
+            this.BTN_PUNTO.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_PUNTO.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_PUNTO.Location = new System.Drawing.Point(152, 380);
+            this.BTN_PUNTO.Name = "BTN_PUNTO";
+            this.BTN_PUNTO.Size = new System.Drawing.Size(64, 64);
+            this.BTN_PUNTO.TabIndex = 30;
+            this.BTN_PUNTO.Text = ".";
+            this.BTN_PUNTO.UseVisualStyleBackColor = false;
+            this.BTN_PUNTO.Click += new System.EventHandler(this.BTN_PUNTO_Click);
+            // 
+            // BTN_0
+            // 
+            this.BTN_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_0.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_0.Location = new System.Drawing.Point(12, 382);
+            this.BTN_0.Name = "BTN_0";
+            this.BTN_0.Size = new System.Drawing.Size(134, 64);
+            this.BTN_0.TabIndex = 31;
+            this.BTN_0.Text = "0";
+            this.BTN_0.UseVisualStyleBackColor = false;
+            this.BTN_0.Click += new System.EventHandler(this.BTN_0_Click);
+            // 
+            // BTN_LIMPIAR
+            // 
+            this.BTN_LIMPIAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_LIMPIAR.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_LIMPIAR.Location = new System.Drawing.Point(82, 102);
+            this.BTN_LIMPIAR.Name = "BTN_LIMPIAR";
+            this.BTN_LIMPIAR.Size = new System.Drawing.Size(134, 64);
+            this.BTN_LIMPIAR.TabIndex = 32;
+            this.BTN_LIMPIAR.Text = "C";
+            this.BTN_LIMPIAR.UseVisualStyleBackColor = false;
+            this.BTN_LIMPIAR.Click += new System.EventHandler(this.BTN_LIMPIAR_Click);
+            // 
+            // BTN_IGUAL
+            // 
+            this.BTN_IGUAL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_IGUAL.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_IGUAL.Location = new System.Drawing.Point(222, 380);
+            this.BTN_IGUAL.Name = "BTN_IGUAL";
+            this.BTN_IGUAL.Size = new System.Drawing.Size(64, 64);
+            this.BTN_IGUAL.TabIndex = 34;
+            this.BTN_IGUAL.Text = "=";
+            this.BTN_IGUAL.UseVisualStyleBackColor = false;
+            this.BTN_IGUAL.Click += new System.EventHandler(this.BTN_IGUAL_Click);
+            // 
+            // BTN_RESTA
+            // 
+            this.BTN_RESTA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_RESTA.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_RESTA.Location = new System.Drawing.Point(222, 242);
+            this.BTN_RESTA.Name = "BTN_RESTA";
+            this.BTN_RESTA.Size = new System.Drawing.Size(64, 64);
+            this.BTN_RESTA.TabIndex = 35;
+            this.BTN_RESTA.Text = "-";
+            this.BTN_RESTA.UseVisualStyleBackColor = false;
+            this.BTN_RESTA.Click += new System.EventHandler(this.BTN_RESTA_Click);
+            // 
+            // BTN_MULTI
+            // 
+            this.BTN_MULTI.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_MULTI.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_MULTI.Location = new System.Drawing.Point(222, 172);
+            this.BTN_MULTI.Name = "BTN_MULTI";
+            this.BTN_MULTI.Size = new System.Drawing.Size(64, 64);
+            this.BTN_MULTI.TabIndex = 36;
+            this.BTN_MULTI.Text = "*";
+            this.BTN_MULTI.UseVisualStyleBackColor = false;
+            this.BTN_MULTI.Click += new System.EventHandler(this.BTN_MULTI_Click);
+            // 
+            // BTN_DIVI
+            // 
+            this.BTN_DIVI.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_DIVI.Font = new System.Drawing.Font("Monotype Corsiva", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_DIVI.Location = new System.Drawing.Point(222, 102);
+            this.BTN_DIVI.Name = "BTN_DIVI";
+            this.BTN_DIVI.Size = new System.Drawing.Size(64, 64);
+            this.BTN_DIVI.TabIndex = 37;
+            this.BTN_DIVI.Text = "/";
+            this.BTN_DIVI.UseVisualStyleBackColor = false;
+            this.BTN_DIVI.Click += new System.EventHandler(this.BTN_DIVI_Click);
+            // 
+            // BTN_SUMA
+            // 
+            this.BTN_SUMA.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_SUMA.Font = new System.Drawing.Font("Monotype Corsiva", 14F, System.Drawing.FontStyle.Italic);
+            this.BTN_SUMA.Location = new System.Drawing.Point(222, 312);
+            this.BTN_SUMA.Name = "BTN_SUMA";
+            this.BTN_SUMA.Size = new System.Drawing.Size(64, 64);
+            this.BTN_SUMA.TabIndex = 38;
+            this.BTN_SUMA.Text = "+";
+            this.BTN_SUMA.UseVisualStyleBackColor = false;
+            this.BTN_SUMA.Click += new System.EventHandler(this.BTN_SUMA_Click);
+            // 
+            // BTN_IMPRIMIR
+            // 
+            this.BTN_IMPRIMIR.BackColor = System.Drawing.Color.Transparent;
+            this.BTN_IMPRIMIR.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BTN_IMPRIMIR.BackgroundImage")));
+            this.BTN_IMPRIMIR.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BTN_IMPRIMIR.FlatAppearance.BorderSize = 0;
+            this.BTN_IMPRIMIR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_IMPRIMIR.Location = new System.Drawing.Point(12, 105);
+            this.BTN_IMPRIMIR.Name = "BTN_IMPRIMIR";
+            this.BTN_IMPRIMIR.Size = new System.Drawing.Size(64, 64);
+            this.BTN_IMPRIMIR.TabIndex = 41;
+            this.BTN_IMPRIMIR.UseVisualStyleBackColor = false;
+            this.BTN_IMPRIMIR.Click += new System.EventHandler(this.BTN_IMPRIMIR_Click);
+            // 
+            // WF_PRINCIPAL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(304, 458);
+            this.Controls.Add(this.BTN_IMPRIMIR);
+            this.Controls.Add(this.BTN_SUMA);
+            this.Controls.Add(this.BTN_DIVI);
+            this.Controls.Add(this.BTN_MULTI);
+            this.Controls.Add(this.BTN_RESTA);
+            this.Controls.Add(this.BTN_IGUAL);
+            this.Controls.Add(this.BTN_LIMPIAR);
+            this.Controls.Add(this.BTN_0);
+            this.Controls.Add(this.BTN_PUNTO);
+            this.Controls.Add(this.BTN_7);
+            this.Controls.Add(this.BTN_8);
+            this.Controls.Add(this.BTN_9);
+            this.Controls.Add(this.BTN_4);
+            this.Controls.Add(this.BTN_5);
+            this.Controls.Add(this.BTN_6);
+            this.Controls.Add(this.BTN_1);
+            this.Controls.Add(this.BTN_2);
+            this.Controls.Add(this.TXT_PANTALLA);
+            this.Controls.Add(this.BTN_3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WF_PRINCIPAL";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BTN_3;
+        private System.Windows.Forms.TextBox TXT_PANTALLA;
+        private System.Windows.Forms.Button BTN_2;
+        private System.Windows.Forms.Button BTN_1;
+        private System.Windows.Forms.Button BTN_6;
+        private System.Windows.Forms.Button BTN_5;
+        private System.Windows.Forms.Button BTN_4;
+        private System.Windows.Forms.Button BTN_9;
+        private System.Windows.Forms.Button BTN_8;
+        private System.Windows.Forms.Button BTN_7;
+        private System.Windows.Forms.Button BTN_PUNTO;
+        private System.Windows.Forms.Button BTN_0;
+        private System.Windows.Forms.Button BTN_LIMPIAR;
+        private System.Windows.Forms.Button BTN_IGUAL;
+        private System.Windows.Forms.Button BTN_RESTA;
+        private System.Windows.Forms.Button BTN_MULTI;
+        private System.Windows.Forms.Button BTN_DIVI;
+        private System.Windows.Forms.Button BTN_SUMA;
+        private System.Windows.Forms.Button BTN_IMPRIMIR;
+    }
+}
+
---
+++ WF_PRINCIPAL/WF_PRINCIPAL.cs
@@ -0,0 +1,280 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System;
+
+namespace WF_PRINCIPAL
+{
+    /// <summary>
+    /// Clase principal del formulario.
+    /// </summary>
+    public partial class WF_PRINCIPAL : Form
+    {
+        // Constructor de la clase
+        public WF_PRINCIPAL()
+        {
+            InitializeComponent();
+        }
+
+        // Variables para almacenar operandos y operador
+        double a = 0;
+        double b = 0;
+        string c = "";
+
+        // Evento para el botón "1"
+        /// <summary>
+        /// Maneja el clic en el botón "1" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_1_Click(object sender, EventArgs e)
+        {
+            TXT_PANTALLA.Text = TXT_PANTALLA.Text + "1";
+        }
+
+        // Evento para el botón "2"
+        /// <summary>
+        /// Maneja el clic en el botón "2" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_2_Click(object sender, EventArgs e)
+        {
+            TXT_PANTALLA.Text = TXT_PANTALLA.Text + "2";
+        }
+
+        // Evento para el botón "3"
+        /// <summary>
+        /// Maneja el clic en el botón "3" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_3_Click(object sender, EventArgs e)
+        {
+            if (TXT_PANTALLA.Text == "")
+            {
+                TXT_PANTALLA.Text = "3";
+            }
+            else
+            {
+                TXT_PANTALLA.Text = TXT_PANTALLA.Text + "3";
+            }
+        }
+
+        // Evento para el botón "4"
+        /// <summary>
+        /// Maneja el clic en el botón "4" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_4_Click(object sender, EventArgs e)
+        {
+            if (TXT_PANTALLA.Text == "")
+            {
+                TXT_PANTALLA.Text = "4";
+            }
+            else
+            {
+                TXT_PANTALLA.Text = TXT_PANTALLA.Text + "4";
+            }
+        }
+
+        // Evento para el botón "5"
+        /// <summary>
+        /// Maneja el clic en el botón "5" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_5_Click(object sender, EventArgs e)
+        {
+            if (TXT_PANTALLA.Text == "")
+            {
+                TXT_PANTALLA.Text = "5";
+            }
+            else
+            {
+                TXT_PANTALLA.Text = TXT_PANTALLA.Text + "5";
+            }
+        }
+
+        // Evento para el botón "6"
+        /// <summary>
+        /// Maneja el clic en el botón "6" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_6_Click(object sender, EventArgs e)
+        {
+            if (TXT_PANTALLA.Text == "")
+            {
+                TXT_PANTALLA.Text = "6";
+            }
+            else
+            {
+                TXT_PANTALLA.Text = TXT_PANTALLA.Text + "6";
+            }
+        }
+
+        // Evento para el botón "7"
+        /// <summary>
+        /// Maneja el clic en el botón "7" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_7_Click(object sender, EventArgs e)
+        {
+            if (TXT_PANTALLA.Text == "")
+            {
+                TXT_PANTALLA.Text = "7";
+            }
+            else
+            {
+                TXT_PANTALLA.Text = TXT_PANTALLA.Text + "7";
+            }
+        }
+
+        // Evento para el botón "8"
+        /// <summary>
+        /// Maneja el clic en el botón "8" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_8_Click(object sender, EventArgs e)
+        {
+            if (TXT_PANTALLA.Text == "")
+            {
+                TXT_PANTALLA.Text = "8";
+            }
+            else
+            {
+                TXT_PANTALLA.Text = TXT_PANTALLA.Text + "8";
+            }
+        }
+
+        // Evento para el botón "9"
+        /// <summary>
+        /// Maneja el clic en el botón "9" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_9_Click(object sender, EventArgs e)
+        {
+            if (TXT_PANTALLA.Text == "")
+            {
+                TXT_PANTALLA.Text = "9";
+            }
+            else
+            {
+                TXT_PANTALLA.Text = TXT_PANTALLA.Text + "9";
+            }
+        }
+
+        // Evento para el botón "0"
+        /// <summary>
+        /// Maneja el clic en el botón "0" y actualiza la pantalla.
+        /// </summary>
+        private void BTN_0_Click(object sender, EventArgs e)
+        {
+            if (TXT_PANTALLA.Text == "")
+            {
+                TXT_PANTALLA.Text = "0";
+            }
+            else
+            {
+                TXT_PANTALLA.Text = TXT_PANTALLA.Text + "0";
+            }
+        }
+
+        // Evento para el botón de división "/"
+        /// <summary>
+        /// Almacena el primer operando, establece el operador, limpia la pantalla y establece el foco.
+        /// </summary>
+        private void BTN_DIVI_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.TXT_PANTALLA.Text);
+            c = "/";
+            this.TXT_PANTALLA.Clear();
+            this.TXT_PANTALLA.Focus();
+        }
+
+        // Evento para el botón de multiplicación "*"
+        /// <summary>
+        /// Almacena el primer operando, establece el operador, limpia la pantalla y establece el foco.
+        /// </summary>
+        private void BTN_MULTI_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.TXT_PANTALLA.Text);
+            c = "*";
+            this.TXT_PANTALLA.Clear();
+            this.TXT_PANTALLA.Focus();
+        }
+
+        // Evento para el botón de resta "-"
+        /// <summary>
+        /// Almacena el primer operando, establece el operador, limpia la pantalla y establece el foco.
+        /// </summary>
+        private void BTN_RESTA_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.TXT_PANTALLA.Text);
+            c = "-";
+            this.TXT_PANTALLA.Clear();
+            this.TXT_PANTALLA.Focus();
+        }
+
+        // Evento para el botón de suma "+"
+        /// <summary>
+        /// Almacena el primer operando, establece el operador y limpia la pantalla.
+        /// </summary>
+        private void BTN_SUMA_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.TXT_PANTALLA.Text);
+            c = "+";
+            this.TXT_PANTALLA.Clear();
+        }
+
+        // Evento para el botón de punto decimal "."
+        /// <summary>
+        /// Agrega un punto decimal a la pantalla si aún no está presente.
+        /// </summary>
+        private void BTN_PUNTO_Click(object sender, EventArgs e)
+        {
+            if (this.TXT_PANTALLA.Text.Contains('.') == false)
+            {
+                this.TXT_PANTALLA.Text = this.TXT_PANTALLA.Text + ".";
+            }
+        }
+
+        // Evento para el botón de igual "="
+        /// <summary>
+        /// Realiza la operación aritmética según el operador almacenado.
+        /// </summary>
+        private void BTN_IGUAL_Click(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(this.TXT_PANTALLA.Text);
+            switch (c)
+            {
+                case "+":
+                    this.TXT_PANTALLA.Text = Convert.ToString(a + b);
+                    break;
+
+                case "-":
+                    this.TXT_PANTALLA.Text = Convert.ToString(a - b);
+                    break;
+
+                case "*":
+                    this.TXT_PANTALLA.Text = Convert.ToString(a * b);
+                    break;
+
+                case "/":
+                    this.TXT_PANTALLA.Text = Convert.ToString(a / b);
+                    break;
+            }
+        }
+
+        // Evento para el botón de imprimir
+        /// <summary>
+        /// Escribe la última operación en un archivo de texto y lo abre.
+        /// </summary>
+        private void BTN_IMPRIMIR_Click(object sender, EventArgs e)
+        {
+            StreamWriter Archivo = new StreamWriter("Historial.txt");
+            Archivo.WriteLine("Última Operación: " + a + c + b + "=" + this.TXT_PANTALLA.Text);
+            Archivo.Close();
+            System.Diagnostics.Process.Start("Historial.txt");
+        }
+
+        // Evento para el botón de limpiar
+        /// <summary>
+        /// Reinicia los operandos y limpia la pantalla.
+        /// </summary>
+        private void BTN_LIMPIAR_Click(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = "";
+            this.TXT_PANTALLA.Text = "";
+        }
+    }
+}
